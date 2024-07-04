4638b299813c0888eea5af36a4ba2fff57c6c318
roshab <roshab@hexabit.in>
2024-07-05 00:45:04 +0530

    added splash screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import {useEffect} from 'react';
 import {StatusBar} from 'react-native';
+import BootSplash from 'react-native-bootsplash';
 import Home from './src/screens/Home';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    BootSplash.hide({fade: true});
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor={'black'} barStyle={'light-content'} />
